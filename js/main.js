fa0042b6290d7a082abd2e3c0823452e71c02ebd
Kenneth <kennethsjorgensen@gmail.com>
2018-06-13 17:17:02 +0200

    Responsiveness

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
-var scroll = new SmoothScroll('a[href*="#"]');
+window.onload = function setFocusToTextBox() {
+    searchBar.focus();
+}
 
 // Barba.Pjax.start();
 
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
+const firstLine = document.querySelector('.first')
+const secondLine = document.querySelector('.second')
+const searchBar = document.querySelector('#search_inputs')
+const dropdown = document.querySelector('.dropdown')
 
+hamburger.addEventListener('mouseup', function(){
+    
+    // firstLine.style.margin = '0'
+    // secondLine.style.margin = '0'
+
+    // firstLine.classList.toggle('collapse')
+    secondLine.classList.toggle('collapse')
+    // menu.classList.toggle('open-menu')
+    
+    
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    searchBar.addEventListener('focus', function () {
+        setTimeout(() => {
+
+            dropdown.classList.add('show-dropdown')
+            
+        }, 500);
+    });
+    searchBar.addEventListener('blur', function(){
+        dropdown.classList.remove('show-dropdown')
+    })
+});
 
 
